7c5501ab185e3cf734068aaaceba311be9af4ebd
mbrit1 <61985946+mbrit1@users.noreply.github.com>
2017-01-25 00:01:46 +0100

    don’t show the post thumbnail on a post. will be only shown when it’s a sticky post

--- development/wordpress_theme/content.php
+++ development/wordpress_theme/content.php
@@ -2,7 +2,6 @@
     <h1 class="uk-article-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
     <p class="uk-article-meta"><?php the_date(); ?></p>
     <div class="uk-clearfix">
-      <img class="uk-align-medium-right article-img" src="<?php the_post_thumbnail_url('large'); ?>" />
       <?php the_content(); ?>
     </div>
 </article>
