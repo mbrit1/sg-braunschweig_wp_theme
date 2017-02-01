be038680add50c18c6fe33dc7605e78d848dede1
mbrit1 <61985946+mbrit1@users.noreply.github.com>
2017-02-01 22:17:07 +0100

    added navigation to sticky posts, added default image to sticky posts and team pages

--- development/wordpress_theme/index.php
+++ development/wordpress_theme/index.php
@@ -1,27 +1,42 @@
 <?php get_header(); ?>
     <!-- Content -->
     <?php $get_important_posts = new WP_Query('cat=4&post_count=5');
+      $number_of_posts = $get_important_posts->found_posts;
       if ($get_important_posts->have_posts()) : ?>
       <!-- Sticky Posts -->
       <div class="sticky-posts uk-block">
         <div class="uk-grid">
           <div class="uk-width-large-6-10 uk-container-center">
-            <ul class="uk-slideshow" data-uk-slideshow="{animation: 'swipe',autoplay:true,autoplayInterval:10000}">
-              <?php while ( $get_important_posts->have_posts() ) : $get_important_posts->the_post(); ?>
-                <li>
-                  <?php if (has_post_thumbnail()) {?>
-                    <img src="<?php the_post_thumbnail_url('large'); ?>">
-                  <?php } else { ?>
-                    <img src="<?php bloginfo('template_directory');?>/images/sticky1.jpeg">
-                  <?php } ?>
-                  <div class="uk-overlay-panel uk-overlay-background uk-overlay-bottom">
-                      <h2><?php the_title(); ?></h2>
-                      <?php the_excerpt(); ?>
-                      <a class="uk-link" href="<?php echo get_permalink(); ?>">mehr lesen</a>
-                  </div>
-                </li>
-            	<?php endwhile; ?>
-            </ul>
+            <div <?php if ( $number_of_posts > 1) echo 'class="uk-slidenav-position";' ?> data-uk-slideshow="{animation: 'swipe',autoplay:true,autoplayInterval:10000}">
+              <ul class="uk-slideshow">
+                <?php while ( $get_important_posts->have_posts() ) : $get_important_posts->the_post(); ?>
+                  <li>
+                    <?php if (has_post_thumbnail()) {?>
+                      <img src="<?php the_post_thumbnail_url('large'); ?>">
+                    <?php } else { ?>
+                      <div class="alt-sticky-img">
+                        <img src="<?php bloginfo('template_directory');?>/images/sg_logo_big.png"  />
+                      </div>
+                    <?php } ?>
+                    <div class="uk-overlay-panel uk-overlay-background uk-overlay-bottom">
+                        <h2><?php the_title(); ?></h2>
+                        <?php the_excerpt(); ?>
+                        <a class="uk-link" href="<?php echo get_permalink(); ?>">mehr lesen</a>
+                    </div>
+                  </li>
+              	<?php endwhile; ?>
+              </ul>
+              <?php if ( $number_of_posts > 1) { ?>
+              <a href="" class="uk-slidenav uk-slidenav-contrast uk-slidenav-previous" data-uk-slideshow-item="previous"></a>
+              <a href="" class="uk-slidenav uk-slidenav-contrast uk-slidenav-next" data-uk-slideshow-item="next"></a>
+              <ul class="uk-dotnav uk-dotnav-contrast uk-position-bottom uk-flex-center">
+                  <li data-uk-slideshow-item="0"><a href=""></a></li>
+                  <li data-uk-slideshow-item="1"><a href=""></a></li>
+              </ul>
+              <?php } ?>
+            </div>
+
+
           </div>
         </div>
       </div>
--- development/wordpress_theme/page-teams.php
+++ development/wordpress_theme/page-teams.php
@@ -3,7 +3,7 @@
 <!-- team page content -->
 <div class="team-cards uk-margin-large">
   <div class="uk-width-9-10 uk-width-large-7-10 uk-container-center">
-    <div class="uk-grid-width-1-2" data-uk-grid="{gutter: 20, animation: false}">
+    <div class="uk-grid-width-small-1-1 uk-grid-width-medium-1-2" data-uk-grid="{gutter: 20, animation: false}">
       <?php
       	$mypages = get_pages( array( 'child_of' => $post->ID, 'sort_column' => 'post_title') );
       	foreach( $mypages as $page ) {
@@ -19,7 +19,9 @@
                   echo get_the_post_thumbnail( $page->ID, 'large' );
                 } else {
               ?>
-                <img src="<?php bloginfo('template_directory');?>/images/team1.jpeg">
+                <div class="alt-sticky-img">
+                  <img src="<?php bloginfo('template_directory');?>/images/sg_logo_small.png"  />
+                </div>
               <?php } ?>
               <h3 class="uk-panel-title"><?php echo $page->post_title; ?></h3>
             </div>
