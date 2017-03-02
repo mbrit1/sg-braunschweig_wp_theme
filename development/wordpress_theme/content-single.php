c2335e796ba767a3aae8485c532792501bc1fc3b
mbrit1 <61985946+mbrit1@users.noreply.github.com>
2017-03-02 18:20:50 +0100

    update content php files to have more meta data and the post thumbnail

--- development/wordpress_theme/content-single.php
+++ development/wordpress_theme/content-single.php
@@ -1,7 +1,15 @@
 <article class="uk-article">
     <h1 class="uk-article-title"><?php the_title(); ?></h1>
-    <p class="uk-article-meta"><?php the_date(); ?></p>
+    <p class="uk-article-meta">Geschrieben von <?php the_author_posts_link(); ?> am <?php the_date(); ?>, <?php the_category( ', ' ); ?></p>
     <div class="uk-clearfix">
-      <?php the_content(); ?>
+      <?php
+        if ( has_post_thumbnail() ) {
+          the_post_thumbnail(array(550,9999), array('class' => 'uk-align-medium-left content-single-img'));
+        }
+        the_content();
+      ?>
+      <p class="uk-text-right">
+        <?php the_tags( 'Tags: ', ', ', '' ); ?>
+      </p>
     </div>
 </article>
--- development/wordpress_theme/content.php
+++ development/wordpress_theme/content.php
@@ -1,8 +1,13 @@
 <article class="uk-article">
     <h1 class="uk-article-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-    <p class="uk-article-meta"><?php the_date(); ?></p>
+    <p class="uk-article-meta">Geschrieben von <?php the_author_posts_link(); ?> am <?php the_date(); ?>, <?php the_category( ', ' ); ?></p>
     <div class="uk-clearfix">
-      <?php the_content(); ?>
+      <?php
+        if ( has_post_thumbnail() ) {
+          the_post_thumbnail(array(550,9999), array('class' => 'uk-align-medium-left content-single-img'));
+        }
+        the_content();
+      ?>
     </div>
 </article>
 <hr class="uk-grid-divider">
